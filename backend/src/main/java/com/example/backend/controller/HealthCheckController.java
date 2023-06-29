beee3234bf24025fc0ecf695ce0c959d38c9a8ce
JunHwan Kim <nefertiri972@gmail.com>
2023-06-30 00:08:01 +0900

    Add health check to Dockerfile
    - 도커 이미지 스크립트에 헬스 체크 추가

---
+++ backend/src/main/java/com/example/backend/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
